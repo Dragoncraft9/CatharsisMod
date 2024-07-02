7ab663de009bd3bb220c4d0438f851957d370251
Dragoncraft9 <118629351+Dragoncraft9@users.noreply.github.com>
2024-07-02 17:30:21 -0500

    hognar2

---
+++ Content/Items/LoreItems/LoreBloodwalker.cs
@@ -0,0 +1,31 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CatharsisMod.Content.Items.LoreItems
+{
+    public class LoreBloodwalker : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.rare = ItemRarityID.LightRed;
+            Item.consumable = false;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ItemID.BloodMoonStarter).
+                AddIngredient(ItemID.SoulofNight, 3).
+                AddTile(TileID.Bookcases).
+                Register();
+        }
+    }
+}
